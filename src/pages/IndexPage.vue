560f80a95c28b6d26f524183bfe69a03041e605c
Ahmad Sadati <a.sadati336@gmail.com>
2024-02-05 15:43:28 +0330

    initial basic

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page padding>
+    <div v-if="messages.length > 0">
+      <div v-for="(message, index) in messages" :key="'message-' + index">
+        {{ message.fullName }}|{{ message.mobile }}<br />
+        {{ message.message }}<br />
+        <hr />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import axios from "axios";
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    const messages = ref([]);
+    function fetchMessages() {
+      axios.get("http://localhost:8000/api/messages").then((r) => {
+        messages.value = r.data;
+      });
+    }
+    fetchMessages();
+    return {
+      messages,
+    };
+  },
+});
+</script>
